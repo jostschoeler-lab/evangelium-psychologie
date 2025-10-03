ca386b082c33e5b5222f18b3f1bc7201f2ffe352
jostschoeler-lab <jost.schoeler@live.no>
2025-10-03 19:31:16 +0200

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const url = import.meta.env.VITE_SUPABASE_URL!;
+const anon = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+
+// ein Client für den Browser
+export const supabase = createClient(url, anon);
