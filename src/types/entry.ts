7ed9f6f8916da1ceb7ab045d2a069b946d134491
jostschoeler-lab <jost.schoeler@live.no>
2025-10-04 13:38:55 +0200

    Create entry.ts

---
+++ src/types/entry.ts
@@ -0,0 +1,13 @@
+// src/types/entry.ts
+export interface Entry {
+  id?: string
+  bibelstellen?: string
+  theologie?: string
+  psych?: string
+  bruecke?: string
+  tags?: string[]
+  sichtbarkeit?: string
+  notiz?: string
+  created_at?: string
+  updated_at?: string
+}
