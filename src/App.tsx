ad86bee1188cef87761e76416145204afbd8f3d7
jostschoeler-lab <jost.schoeler@live.no>
2025-10-04 20:17:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import EpEditorPage from "./pages/EpEditorPage";
 
 export default function App() {
+  console.log("APP_MINIMAL v3"); // debug marker
   return <EpEditorPage />;
 }
