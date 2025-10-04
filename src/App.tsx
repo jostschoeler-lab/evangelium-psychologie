60f8bc0dcd86a5d2b4e6f6ced9f6939941f76c4d
jostschoeler-lab <jost.schoeler@live.no>
2025-10-04 22:51:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+
+// Seiten importieren
 import Menu from "./pages/Menu";
 import NBJ from "./pages/NBJ";
 import EP from "./pages/EP";
+import Spannungsmodell from "./pages/Spannungsmodell";
+import Stuhldialog from "./pages/Stuhldialog";
+import Bibliothek from "./pages/Bibliothek";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
+        {/* Startmenü */}
         <Route path="/" element={<Menu />} />
+
+        {/* Module */}
         <Route path="/nbj" element={<NBJ />} />
         <Route path="/ep" element={<EP />} />
+        <Route path="/spannungsmodell" element={<Spannungsmodell />} />
+        <Route path="/stuhldialog" element={<Stuhldialog />} />
+        <Route path="/bibliothek" element={<Bibliothek />} />
       </Routes>
     </HashRouter>
   );
