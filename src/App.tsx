1c635e7c3f7b277998d1cdb7aa708b0aa26bc944
jostschoeler-lab <jost.schoeler@live.no>
2025-10-04 19:46:17 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,5 @@ import React from "react";
 import EpEditorPage from "./pages/EpEditorPage";
 
 export default function App() {
-  return (
-    <>
-      <div style={{background:"#fffae6",border:"1px solid #e3c200",padding:8,margin:"8px 0"}}>
-        *** MARKER A: App.tsx aktiv ***
-      </div>
-      <EpEditorPage />
-    </>
-  );
+  return <EpEditorPage />;
 }
