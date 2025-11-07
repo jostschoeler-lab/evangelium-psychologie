9619debd0ae9099e48185911855edfb95fec5474
jostschoeler-lab <jost.schoeler@live.no>
2025-11-07 01:54:16 +0100

    Adjust intro image to Kind mit Panzer

--- src/pages/Bibliothek.tsx
+++ src/pages/Bibliothek.tsx
@@ -1773,8 +1773,8 @@ export default function Bibliothek() {
           }}
         >
           <img
-            src="/bibliothek/verwandlung-intro.svg"
-            alt="Jesus hält ein weinendes Kind im Arm"
+            src="/Images/kindmitpanzer.png"
+            alt="Illustration eines Kindes mit Panzer"
             style={{ width: "100%", height: "100%", objectFit: "cover" }}
           />
         </div>
