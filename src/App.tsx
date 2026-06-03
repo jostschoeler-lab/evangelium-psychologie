3f0cf5788b22e45b3591dde4519833fa37d5ba22
jostschoeler-lab <jost.schoeler@live.no>
2026-06-03 15:36:13 +0200

    Add Repression or Transformation page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Spannungsmodell from "./pages/Spannungsmodell";
 import Bibliothek from "./pages/Bibliothek";
 import EP from "./pages/EP";
 import Stuhldialog from "./pages/Stuhldialog";
+import RepressionOrTransformation from "./pages/RepressionOrTransformation";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Route path="/Bedürfnisse" element={<Bibliothek />} />
       <Route path="/ep" element={<EP />} />
       <Route path="/stuhldialog" element={<Stuhldialog />} />
+      <Route path="/repression-or-transformation" element={<RepressionOrTransformation />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
